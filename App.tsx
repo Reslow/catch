1c8a2edae925cde55ff2d2e5e9b7952c91d59de7
Fanny Reslow <reslowf@gmail.com>
2022-08-01 13:39:51 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{ width: 100, height: 100 }} />
+      <Text style={styles.fontStyle}>Welome</Text>
     </View>
   );
 }
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fontStyle: {
+    color: "#923",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
